03f8b58a35a1d2d9846c82a9ccbbc3062e4c4ff7
joeymalope-za <94571003+Joemalope@users.noreply.github.com>
2022-09-05 05:20:18 +0200

    Update variable name to improve readabilty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent {
   } 
 
   generateQuote(e:any){
-    let input = this.orderNumber;
+    let orderNo = this.orderNumber;
     //Prevent default button behavior
     e.stopPropagation();
     e.preventDefault();
@@ -41,37 +41,37 @@ export class AppComponent {
     this.reset();
     this.showReceipt = true;
     //Calculate the boxes and their sizes and store the results in index tracker
-    this.calculatePacketsV4(input);
+    this.calculatePacketsV4(orderNo);
     //print results on the Quotation
   }
 
   //Can only support an array of size 5
-  calculatePacketsV2(input:number){
-    if (input == 0) {
+  calculatePacketsV2(orderNo:number){
+    if (orderNo == 0) {
       //base case
       return 0;
       //in between cases
-    } else if (this.widgetPacketSizes[4] <= input) {
-      this.calculatePacketsV2(input - this.widgetPacketSizes[4]);
+    } else if (this.widgetPacketSizes[4] <= orderNo) {
+      this.calculatePacketsV2(orderNo - this.widgetPacketSizes[4]);
       this.indexTracker[4].count++;
-    } else if (this.widgetPacketSizes[3] <= input) {
-      this.calculatePacketsV2(input - this.widgetPacketSizes[3]);
+    } else if (this.widgetPacketSizes[3] <= orderNo) {
+      this.calculatePacketsV2(orderNo - this.widgetPacketSizes[3]);
       this.indexTracker[3].count++;
-    } else if (this.widgetPacketSizes[2] <= input) {
-      this.calculatePacketsV2(input - this.widgetPacketSizes[2]);
+    } else if (this.widgetPacketSizes[2] <= orderNo) {
+      this.calculatePacketsV2(orderNo - this.widgetPacketSizes[2]);
       this.indexTracker[2].count++;
-    } else if (this.widgetPacketSizes[1] <= input) {
-      this.calculatePacketsV2(input - this.widgetPacketSizes[1]);
+    } else if (this.widgetPacketSizes[1] <= orderNo) {
+      this.calculatePacketsV2(orderNo - this.widgetPacketSizes[1]);
       this.indexTracker[1].count++;
     } else if (
       //last element
-      this.widgetPacketSizes[0] <= input &&
-      input - this.widgetPacketSizes[0] < this.widgetPacketSizes[0] &&
-      input - this.widgetPacketSizes[0] > 0
+      this.widgetPacketSizes[0] <= orderNo &&
+      orderNo - this.widgetPacketSizes[0] < this.widgetPacketSizes[0] &&
+      orderNo - this.widgetPacketSizes[0] > 0
     ) {
       this.indexTracker[1].count++;
     } // smaller than last element
-    else if (this.widgetPacketSizes[0] >= input) {
+    else if (this.widgetPacketSizes[0] >= orderNo) {
       this.indexTracker[0].count++;
     }
 
@@ -79,41 +79,40 @@ export class AppComponent {
   };
 
   //This can calculated the size regardless of the number of elements in the widgetPacketSizes.
-  calculatePacketsV4(input:number ){
-    if (input == 0) {
+  calculatePacketsV4(orderNo:number ){
+    if (orderNo == 0) {
       //base case
       return 0;
     } else if (
-      this.widgetPacketSizes[0] <= input &&
-      !(input - this.widgetPacketSizes[0] <= this.widgetPacketSizes[0] && input - this.widgetPacketSizes[0] > 0)
+      this.widgetPacketSizes[0] <= orderNo &&
+      !(orderNo - this.widgetPacketSizes[0] <= this.widgetPacketSizes[0] && orderNo - this.widgetPacketSizes[0] > 0)
     ) {
       //in between cases
       let index = -1;
       for (let i = this.widgetPacketSizes.length - 1; i >= 0; i--) {
         if (
           i == 0 &&
-          input - this.widgetPacketSizes[0] <= this.widgetPacketSizes[0] &&
-          input - this.widgetPacketSizes[0] > 0
+          orderNo - this.widgetPacketSizes[0] <= this.widgetPacketSizes[0] &&
+          orderNo - this.widgetPacketSizes[0] > 0
         ) {
           // exit case
           this.indexTracker[i + 1].count++;
-        } else if (this.widgetPacketSizes[i] <= input) {
+        } else if (this.widgetPacketSizes[i] <= orderNo) {
           index = i;
           break;
         }
       }
-      this.calculatePacketsV4(input - this.widgetPacketSizes[index]);
-      this.indexTracker[index].count++;
-    } else if (
-      //last element
-      this.widgetPacketSizes[0] <= input &&
-      input - this.widgetPacketSizes[0] < this.widgetPacketSizes[0] &&
-      input - this.widgetPacketSizes[0] > 0
+      this.calculatePacketsV4((orderNo - this.widgetPacketSizes[index]));
+      if(index!=-1)
+        this.indexTracker[index].count++;
+    } else if ( //accomadate edge case there (orderNo == pack Size)
+      this.widgetPacketSizes[0] <= orderNo &&
+      orderNo - this.widgetPacketSizes[0] > 0
     ) {
       this.indexTracker[1].count++;
       this.calculatePacketsV4(0);
     } // smaller than last element
-    else if (this.widgetPacketSizes[0] >= input) {
+    else if (this.widgetPacketSizes[0] >= orderNo) {
       this.indexTracker[0].count++;
     }
 
